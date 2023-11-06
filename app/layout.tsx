c8c6a440d7c5d8599f3e010b99643bd7d1ab3f63
Gabriel Tinoco <gabrielslai@gmail.com>
2023-11-06 11:48:10 -0500

    Updated Navbar component with auth support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,34 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
-import NavBar from '@/components/NavBar';
+import Navbar from '@/components/navbar/navbar';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+import { PropsWithChildren } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Healthy Harvest',
-  description:
-    'A recipe builder for healthy eaters. User can browse various recipe made by other chiefs that meets their diet and nutritional needs.',
+	title: 'Healthy Harvest',
+	description:
+		'A recipe builder for healthy eaters. User can browse various recipe made by other chiefs that meets their diet and nutritional needs.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <Providers>
-          <NavBar />
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
+export default async function RootLayout({ children }: PropsWithChildren) {
+	// Returns decoded authentication token
+	function getAuthToken() {
+		const jwtToken = cookies().get('token');
+		return jwt.decode(jwtToken?.value as string);
+	}
+
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<Providers>
+					<Navbar jwtToken={getAuthToken()} />
+					{children}
+				</Providers>
+			</body>
+		</html>
+	);
 }
