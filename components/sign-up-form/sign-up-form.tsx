8a3aaa76a8559440caffeaf57bc161804dfd2a3d
Gabriel Tinoco <gabrielslai@gmail.com>
2023-11-06 11:49:06 -0500

    Improved login/sign-up forms and components

--- components/sign-up-form/sign-up-form.tsx
+++ components/sign-up-form/sign-up-form.tsx
@@ -1,17 +1,28 @@
 import { signUpUser } from '@/server/actions/user-actions';
 import { Button, Input } from '@nextui-org/react';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useRef, useState } from 'react';
 
 export default function SignUpForm() {
+	// HTML form element reference
+	const formElementRef = useRef<HTMLFormElement>(null);
+
+	// Error message
 	const [errorMessage, setErrorMessage] = useState<String>('‎ ');
 
+	// Sign up form handler function
 	async function signUpFormHandler(formData: FormData) {
 		const serverResponse = await signUpUser(formData);
 		if (serverResponse) setErrorMessage(serverResponse.message);
+		else {
+			formElementRef.current?.reset();
+			setErrorMessage('‎ ');
+		}
 	}
 
 	return (
 		<form
+			ref={formElementRef}
 			className='flex flex-col gap-2'
 			action={signUpFormHandler}>
 			<div className='flex gap-2'>
@@ -62,7 +73,7 @@ export default function SignUpForm() {
 			/>
 
 			{/* Form error message */}
-			<p>{errorMessage}</p>
+			<p className='text-red-500'>{errorMessage}</p>
 
 			{/* Sign up button */}
 			<Button type='submit'>Sign Up</Button>
