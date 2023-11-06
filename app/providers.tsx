ed73a57824f6a172a213eba200ae734b94a4e2bd
Gabriel Tinoco <gabrielslai@gmail.com>
2023-11-06 11:47:19 -0500

    Created and implemented auth React context

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,12 @@
 
 import React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
+import AuthContextProvider from '@/context/auth-context';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
+	return (
+		<NextUIProvider>
+			<AuthContextProvider>{children}</AuthContextProvider>
+		</NextUIProvider>
+	);
 };
