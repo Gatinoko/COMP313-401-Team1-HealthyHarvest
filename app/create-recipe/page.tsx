b4fa3750dd1b65622853cc45b3d302752e3dc662
Aun-Raza <aunrazaq123@gmail.com>
2023-11-14 10:52:31 -0500

    Create recipe builder app, and make navbar brand clickable

---
+++ app/create-recipe/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+export default function CreateRecipe() {
+  return (
+    <main className='mt-4 container mx-auto flex flex-col gap-4'>
+      <h1 className='text-5xl'>Create Recipe</h1>
+    </main>
+  );
+}
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,18 +2,18 @@
 
 import React, { useEffect, useState } from 'react';
 import {
-	Navbar as Navigation,
-	NavbarBrand,
-	NavbarContent,
-	NavbarItem,
-	Button,
-	Link,
-	Dropdown,
-	DropdownTrigger,
-	Avatar,
-	DropdownMenu,
-	DropdownItem,
-	Input,
+  Navbar as Navigation,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+  Link,
+  Dropdown,
+  DropdownTrigger,
+  Avatar,
+  DropdownMenu,
+  DropdownItem,
+  Input,
 } from '@nextui-org/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -23,122 +23,115 @@ import { useContext } from 'react';
 import { AuthContext } from '@/context/auth-context';
 
 type NavigationProps = {
-	jwtToken?: any;
+  jwtToken?: any;
 };
 
 export default function Navbar({ jwtToken }: NavigationProps) {
-	// Router reference
-	const router = useRouter();
+  // Router reference
+  const router = useRouter();
 
-	// Auth context information
-	const { authInformation, setAuthInformation } = useContext(AuthContext);
+  // Auth context information
+  const { authInformation, setAuthInformation } = useContext(AuthContext);
 
-	// Logout button handler function
-	async function logoutButtonHandler() {
-		await fetch('http://localhost:3000/api/auth', {
-			method: 'DELETE',
-		});
-		router.refresh();
-	}
+  // Logout button handler function
+  async function logoutButtonHandler() {
+    await fetch('http://localhost:3000/api/auth', {
+      method: 'DELETE',
+    });
+    router.refresh();
+  }
 
-	useEffect(() => {
-		// If token is present, update the auth context
-		if (jwtToken)
-			setAuthInformation({
-				isAuthenticated: true,
-				firstName: jwtToken.firstName,
-				lastName: jwtToken.lastName,
-				email: jwtToken.email,
-				username: jwtToken.username,
-			});
-	}, [jwtToken, setAuthInformation]);
+  useEffect(() => {
+    // If token is present, update the auth context
+    if (jwtToken)
+      setAuthInformation({
+        isAuthenticated: true,
+        firstName: jwtToken.firstName,
+        lastName: jwtToken.lastName,
+        email: jwtToken.email,
+        username: jwtToken.username,
+      });
+  }, [jwtToken, setAuthInformation]);
 
-	return (
-		<Navigation>
-			<NavbarBrand>
-				<div className='w-8 h-8 bg-green-400 rounded-md me-2'></div>
-				<p className='font-bold text-inherit'>Healthy Harvest</p>
-			</NavbarBrand>
+  return (
+    <Navigation>
+      <NavbarBrand as={Link} href='/'>
+        <div className='w-8 h-8 bg-green-400 rounded-md me-2'></div>
+        <p className='font-bold text-inherit'>Healthy Harvest</p>
+      </NavbarBrand>
 
-			<NavbarContent
-				justify='end'
-				className='flex gap-4'>
-				{jwtToken ? (
-					<>
-						{/* Search input */}
-						<NavbarItem className='hidden lg:flex'>
-							<div className='flex items-center gap-2'>
-								<FontAwesomeIcon
-									size='lg'
-									icon={faMagnifyingGlass}
-								/>
-								<Input
-									className='border rounded-xl'
-									placeholder='Salads, bean soup...'
-								/>
-							</div>
-						</NavbarItem>
+      <NavbarContent justify='end' className='flex gap-4'>
+        {jwtToken ? (
+          <>
+            {/* Search input */}
+            <NavbarItem className='hidden lg:flex'>
+              <div className='flex items-center gap-2'>
+                <FontAwesomeIcon size='lg' icon={faMagnifyingGlass} />
+                <Input
+                  className='border rounded-xl'
+                  placeholder='Salads, bean soup...'
+                />
+              </div>
+            </NavbarItem>
 
-						{/* Avatar with dropdown menu */}
-						<Dropdown placement='bottom-end'>
-							{/* Avatar */}
-							<DropdownTrigger>
-								<Avatar
-									isBordered
-									as='button'
-									className='transition-transform'
-									size='sm'
-									src='https://i.pravatar.cc/150?u=a042581f4e29026704d'
-								/>
-							</DropdownTrigger>
+            {/* Avatar with dropdown menu */}
+            <Dropdown placement='bottom-end'>
+              {/* Avatar */}
+              <DropdownTrigger>
+                <Avatar
+                  isBordered
+                  as='button'
+                  className='transition-transform'
+                  size='sm'
+                  src='https://i.pravatar.cc/150?u=a042581f4e29026704d'
+                />
+              </DropdownTrigger>
 
-							{/* Dropdown menu */}
-							<DropdownMenu
-								aria-label='Profile Actions'
-								variant='flat'>
-								{/* User profile */}
-								<DropdownItem
-									key='profile'
-									className='h-14 gap-2'>
-									<p className='font-semibold'>
-										{`Welcome, ${authInformation.username}!`}
-									</p>
-									<p className=' text-green-500'>zoey@example.com</p>
-								</DropdownItem>
+              {/* Dropdown menu */}
+              <DropdownMenu aria-label='Profile Actions' variant='flat'>
+                {/* User profile */}
+                <DropdownItem key='profile' className='h-14 gap-2'>
+                  <p className='font-semibold'>
+                    {`Welcome, ${authInformation.username}!`}
+                  </p>
+                  <p className=' text-green-500'>{authInformation.email}</p>
+                </DropdownItem>
+                {/* Create Recipe */}
+                <DropdownItem key='create-recipe'>
+                  <Button
+                    className='flex justify-center'
+                    as={Link}
+                    href='/create-recipe'
+                    color='primary'
+                  >
+                    Create Recipe
+                  </Button>
+                </DropdownItem>
+                {/* Logout button */}
+                <DropdownItem key='logout' onClick={logoutButtonHandler}>
+                  Log Out
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </>
+        ) : (
+          <>
+            {/* Sign-up page button */}
+            <NavbarItem>
+              <Button as={Link} href='/sign-up' variant='flat'>
+                Sign Up
+              </Button>
+            </NavbarItem>
 
-								{/* Logout button */}
-								<DropdownItem
-									key='logout'
-									onClick={logoutButtonHandler}>
-									Log Out
-								</DropdownItem>
-							</DropdownMenu>
-						</Dropdown>
-					</>
-				) : (
-					<>
-						{/* Sign-up page button */}
-						<NavbarItem>
-							<Button
-								as={Link}
-								href='/sign-up'
-								variant='flat'>
-								Sign Up
-							</Button>
-						</NavbarItem>
-
-						{/* Login page button */}
-						<NavbarItem>
-							<Button
-								as={Link}
-								href='/login'
-								variant='flat'>
-								Login
-							</Button>
-						</NavbarItem>
-					</>
-				)}
-			</NavbarContent>
-		</Navigation>
-	);
+            {/* Login page button */}
+            <NavbarItem>
+              <Button as={Link} href='/login' variant='flat'>
+                Login
+              </Button>
+            </NavbarItem>
+          </>
+        )}
+      </NavbarContent>
+    </Navigation>
+  );
 }
