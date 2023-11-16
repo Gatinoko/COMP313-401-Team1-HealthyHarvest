6e32c2cc1b7b77b789ea148144e6c328a416676e
Richie Gecko <richie.hickey513@gmail.com>
2023-11-16 13:01:34 -0400

    Added recipe model to Prisma schema

---
+++ server/schemas/recipe-schemas.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+/**
+ * Zod schema for ingredients object 
+ */
+const ingredientSchema = z.object({
+    text: z.string(),
+});
+
+/**
+ * Zod schema for directions object
+ */
+const directionSchema = z.object({
+    step: z.string(),
+});
+
+/**
+*Zod schema for creating a recipe
+*/
+export const createRecipeSchema = z.object({
+    title: z.string(),
+    imageUrl: z.string().optional(),
+    desc: z.string().optional(),
+    servings: z.number().optional(),
+    yield: z.string().optional(),
+    prepTime: z.string().optional(),
+    cookTime: z.string().optional(),
+    public: z.boolean(),
+    ingredients: z.array(ingredientSchema),
+    directions: z.array(directionSchema)
+});
