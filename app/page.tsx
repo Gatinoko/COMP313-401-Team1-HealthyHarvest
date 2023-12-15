70adcc46e168d91d8d0fa76a4e6358d291219e62
Gabriel Tinoco <gabrielslai@gmail.com>
2023-12-15 10:48:26 -0500

    Made home a server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+'use server';
+
 import { RecipeItemList } from '@/components/recipe-item-list/recipe-item-list';
 
-export default function Home() {
+export default async function Home() {
 	return (
 		<main className='mt-4 container mx-auto'>
 			<h1 className='text-4xl text-center font-bold'>All Recipes</h1>
