60202fdf7f6ba13d800fd5ed7fac55553aac99b4
Gabriel Tinoco <gabrielslai@gmail.com>
2023-12-15 14:20:38 -0500

    Implemented userId prop in RecipeItemList component

--- components/recipe-item-list/recipe-item-list.tsx
+++ components/recipe-item-list/recipe-item-list.tsx
@@ -1,28 +1,44 @@
 'use server';
 
 import { RecipeItem } from './recipe-item';
-import { getAllRecipes } from '@/server/actions/recipe-actions';
+import {
+	getAllRecipes,
+	getRecipesByUser,
+} from '@/server/actions/recipe-actions';
 
-export type RecipeItemListProps = {};
+export type RecipeItemListProps = {
+	userId?: string;
+};
 
-export async function RecipeItemList(props: RecipeItemListProps) {
-	const allRecipes = await getAllRecipes();
+export async function RecipeItemList({
+	userId = undefined,
+}: RecipeItemListProps) {
+	// Either all recipes, or just a user's recipes
+	const recipes = (await assignRecipes())!;
 
-	console.log(allRecipes);
+	// Function which returns an user's recipes if the userId prop is defined, or all recipes if it's left undefined
+	async function assignRecipes() {
+		if (userId) {
+			const userRecipes = await getRecipesByUser(userId);
+			if (Array.isArray(userRecipes)) return userRecipes;
+		} else {
+			const allRecipes = await getAllRecipes();
+			if (Array.isArray(allRecipes)) return allRecipes;
+		}
+	}
 
 	return (
 		<ul className='grid grid-cols-3 mt-4 gap-2'>
-			{Array.isArray(allRecipes) &&
-				allRecipes.map(({ id, title, imageUrl, user, reviews }, index) => (
-					<RecipeItem
-						key={index}
-						id={id}
-						title={title}
-						user={user}
-						reviews={reviews}
-						imageUrl={imageUrl ? imageUrl : '/recipeBeef.webp'}
-					/>
-				))}
+			{recipes.map(({ id, title, imageUrl, user, reviews }, index) => (
+				<RecipeItem
+					key={index}
+					id={id}
+					title={title}
+					user={user}
+					reviews={reviews}
+					imageUrl={imageUrl ? imageUrl : '/recipeBeef.webp'}
+				/>
+			))}
 		</ul>
 	);
 }
